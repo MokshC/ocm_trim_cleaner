be5f752d2317271e80784f3e7bf003e8e5d77a6a
Moksh <mokshchitkara@gmail.com>
2025-12-23 23:02:33 -0800

    Update to v1.0.4: fixed handling of red media for edge case

--- ocm_trim_cleaner.py
+++ ocm_trim_cleaner.py
@@ -1,6 +1,6 @@
 # Moksh Chitkara
-# OCM Trim Cleaner v1.0.3
-# Last Updated: Dec 17th 2025
+# OCM Trim Cleaner v1.0.4
+# Last Updated: Dec 23th 2025
 
 import os
 import sys
@@ -69,12 +69,14 @@ def task_out(path, verbose):
     return tasks
 
 # just shutil move but for threads
-def move(item, new_dir):
+def move(item, new_dir, verbose):
     path = new_dir / item.name
     if os.path.exists(path):
-        raise ValueError(f"The file '{path}' already exists.")    
+        raise ValueError(f"Trying to move The file '{item}' but '{path}' already exists. New dir: {new_dir}")    
     else:
         shutil.move(str(item), str(path))
+        if verbose:
+            print(f"Moved {item} -> {path}")
         return path
 
 # Returns True if len iterable greater than 1
@@ -96,8 +98,10 @@ def iter_over_one(i):
 # output: none
 def cleanup(path, verbose):
 
+    print("ITER DER", path, iter_over_one(path.iterdir()))
+
     # If it is an image sequence
-    if path.is_dir() and (iter_over_one(path.iterdir())):
+    if path.is_dir() and not str(path).endswith(".RDC"):
         new_dir_name = re.sub(r'_S\d{3}$', '', path.name)   # get new name
         new_dir = path.parent / new_dir_name                # create new directory path
         os.makedirs(new_dir, exist_ok=True)                 # create directory on system
@@ -111,9 +115,7 @@ def cleanup(path, verbose):
         thread_finish(t, threads)
     
         path.rmdir()                                        # delete old directory
-    
-        if verbose:
-            print(f"Moved {path} -> {new_dir}")
+
         return 
 
     matches = re.findall(r'_S\d+', path.name)           # get _S### out of name
@@ -121,26 +123,30 @@ def cleanup(path, verbose):
     os.makedirs(new_dir, exist_ok=True)                 # create directory on system
     
     # If it is a R3D file
-    if path.is_dir() and not (iter_over_one(path.iterdir())):
+    if path.is_dir() and str(path).endswith(".RDC"):
         new_dir = new_dir / (re.sub(r'_S\d{3}', '', path.name))     # recreate og subdirectory without _S###
         os.makedirs(new_dir, exist_ok=True)                         # create subdirectory on system
         
-        move(next(path.iterdir()), new_dir)                     # move the file
-        path.rmdir()                                            # delete old directory
-    
-        if verbose:
-            print(f"Moved {path} -> {new_dir}")
+        if (iter_over_one(path.iterdir())):
+            # move everything using multithreading
+            threads = []
+            for item in path.iterdir():                         
+                t = threading.Thread(target=move, args=(item, new_dir))
+                threads.append(t)
+            thread_finish(t, threads)
+        else:
+            move(next(path.iterdir()), new_dir, verbose)            # move the file
+        
+        path.rmdir()                                                # delete old directory
+
     
     # if it is a video file
     else:
-        moved_path = move(path, new_dir)                        # move the item
+        moved_path = move(path, new_dir, verbose)               # move the item
         
-        new_file_name = re.sub(r'_S\d{3}', '', moved_path.name)   # rename it removing S###
+        new_file_name = re.sub(r'_S\d{3}', '', moved_path.name) # rename it removing S###
         renamed_path = moved_path.parent / new_file_name        # create path
         moved_path.rename(renamed_path)                         # do the rename on system
-
-        if verbose:
-            print(f"Moved {path} -> {renamed_path}")
 
 if __name__ == "__main__":
     path, verbose = ask_user()  # get path and verbose preference
